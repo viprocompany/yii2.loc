f2f5d92ac0a05090d5f73b52e7df0a2503527fba
vitali <vitali_igorevich_doc@mail.ru>
2021-04-13 15:11:09 +0300

    Update yii2_loc_dump.sql
    NEW yii2_loc_dump.sql
    login: admin@mail.ru
    password: 1111

--- yii2_loc_dump.sql
+++ yii2_loc_dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Апр 01 2021 г., 20:40
+-- Время создания: Апр 13 2021 г., 15:05
 -- Версия сервера: 5.7.29
 -- Версия PHP: 7.4.14
 
@@ -54,7 +54,8 @@ INSERT INTO `category` (`id`, `parent_id`, `title`, `description`, `keywords`) V
 (12, 0, 'Frozen Foods', NULL, NULL),
 (13, 12, 'Frozen Snacks', NULL, NULL),
 (14, 12, 'Frozen Nonveg', NULL, NULL),
-(15, 0, 'Bread & Bakery', NULL, NULL);
+(15, 0, 'Bread & Bakery', NULL, NULL),
+(16, 0, 'Категория 1', 'Категория 1', '');
 
 -- --------------------------------------------------------
 
@@ -81,9 +82,6 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `created_at`, `updated_at`, `qty`, `total`, `status`, `name`, `email`, `phone`, `address`, `note`) VALUES
-(31, '2021-03-30 18:23:57', '2021-03-30 18:23:57', 5, '10.00', 0, 'Тест', 'p.vit2009@yandex.ru', '77777777777777', 'Город', ''),
-(32, '2021-03-30 18:28:07', '2021-03-30 18:28:07', 1, '5.00', 0, 'Тест', 'vitalijbitrix@gmail.com', '77777777777777', 'Город', ''),
-(33, '2021-03-30 18:41:50', '2021-03-30 18:41:50', 2, '6.00', 0, 'Тест', 'p.vit2009@yandex.ru', '77777777777777', 'Город', ''),
 (34, '2021-03-30 18:47:32', '2021-03-30 18:47:32', 1, '5.00', 0, 'Тестер', 'p.vit2009@yandex.ru', '77777777777777', 'Город', ''),
 (35, '2021-03-30 18:57:13', '2021-03-30 18:57:13', 1, '5.00', 0, 'Тест', 'p.vit2009@yandex.ru', '77777777777777', 'Город', ''),
 (36, '2021-03-30 19:07:22', '2021-03-30 19:07:22', 2, '10.00', 0, 'Василий', 'p.vit2009@yandex.ru', '77777777777777', 'Город', ''),
@@ -93,8 +91,11 @@ INSERT INTO `orders` (`id`, `created_at`, `updated_at`, `qty`, `total`, `status`
 (40, '2021-04-01 12:42:14', '2021-04-01 19:13:30', 6, '25.00', 0, 'Петров', 'p.vit2009@yandex.ru', '+1111111', 'Минск', 'Клиент:  Самовывоз.\r\nМенеджер:  из магазина или склада?'),
 (41, '2021-04-01 12:43:24', '2021-04-01 12:43:24', 4, '12.00', 0, 'Сидоров', 'p.vit2009@yandex.ru', '+1234567', 'Одесса', 'Шоб ви были здоровы'),
 (42, '2021-04-01 12:44:10', '2021-04-01 12:44:10', 1, '3.00', 0, 'Иванов ', 'p.vit2009@yandex.ru', '+5555555', 'Москва', ''),
-(43, '2021-04-01 13:35:01', '2021-04-01 16:09:06', 4, '15.00', 1, 'Виталий', 'p.vit2009@yandex.ru', '77777777777777', 'Могилёв', ''),
-(44, '2021-04-01 13:37:09', '2021-04-01 13:37:09', 2, '6.00', 0, 'Василий', 'p.vit2009@yandex.ru', '77777777777777', 'Могилёв', '');
+(43, '2021-04-01 13:35:01', '2021-04-01 21:15:25', 4, '15.00', 1, 'Виталий', 'p.vit2009@yandex.ru', '777777777777', 'Могилёв', ''),
+(44, '2021-04-01 13:37:09', '2021-04-01 13:37:09', 2, '6.00', 0, 'Василий', 'p.vit2009@yandex.ru', '77777777777777', 'Могилёв', ''),
+(45, '2021-04-04 17:23:35', '2021-04-04 17:23:35', 2, '6.00', 1, 'Тест', 'mail@mail.mail', '+1111111', 'Одесса', ''),
+(46, '2021-04-06 22:31:41', '2021-04-06 22:31:41', 8, '33.00', 0, 'Иван Петров', 'mail@mail.mail', '+123456789', 'Москва,  3-я ул. Строителей, 25', 'Самовывоз'),
+(47, '2021-04-06 22:55:59', '2021-04-06 22:55:59', 1, '3.00', 0, 'Иван Петров', 'mail@mail.mail', '+123456789', 'Москва,  3-я ул. Строителей, 25', '!!!!!!!!!!!!!!!!!!!');
 
 -- --------------------------------------------------------
 
@@ -117,9 +118,6 @@ CREATE TABLE `order_product` (
 --
 
 INSERT INTO `order_product` (`id`, `order_id`, `product_id`, `title`, `price`, `qty`, `total`) VALUES
-(40, 31, 5, 'fresh spinach (palak)', '2.00', 5, '10.00'),
-(41, 32, 4, 'premium bake rusk (300 gm)', '5.00', 1, '5.00'),
-(42, 33, 1, 'knorr instant soup (100 gm)', '3.00', 2, '6.00'),
 (43, 34, 6, 'fresh mango dasheri (1 kg)', '5.00', 1, '5.00'),
 (44, 35, 2, 'chings noodles (75 gm)', '5.00', 1, '5.00'),
 (45, 36, 6, 'fresh mango dasheri (1 kg)', '5.00', 2, '10.00'),
@@ -139,7 +137,14 @@ INSERT INTO `order_product` (`id`, `order_id`, `product_id`, `title`, `price`, `
 (59, 43, 8, 'fresh broccoli (500 gm)', '4.00', 1, '4.00'),
 (60, 43, 5, 'fresh spinach (palak)', '2.00', 1, '2.00'),
 (61, 43, 10, 'prune juice - sunsweet (1 ltr)', '4.00', 1, '4.00'),
-(62, 44, 9, 'mixed fruit juice (1 ltr)', '3.00', 2, '6.00');
+(62, 44, 9, 'mixed fruit juice (1 ltr)', '3.00', 2, '6.00'),
+(63, 45, 3, 'lahsun sev (150 gm)', '3.00', 2, '6.00'),
+(64, 46, 3, 'lahsun sev (150 gm)', '3.00', 1, '3.00'),
+(65, 46, 4, 'premium bake rusk (300 gm)', '5.00', 1, '5.00'),
+(66, 46, 5, 'fresh spinach (palak)', '2.00', 2, '4.00'),
+(67, 46, 20, 'test product 3', '7.00', 3, '21.00'),
+(68, 46, 21, 'test product', '0.00', 1, '0.00'),
+(69, 47, 3, 'lahsun sev (150 gm)', '3.00', 1, '3.00');
 
 -- --------------------------------------------------------
 
@@ -153,10 +158,10 @@ CREATE TABLE `product` (
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
   `price` decimal(6,2) NOT NULL DEFAULT '0.00',
-  `old_price` decimal(6,2) NOT NULL DEFAULT '0.00',
+  `old_price` decimal(6,2) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `keywords` varchar(255) DEFAULT NULL,
-  `img` varchar(255) NOT NULL DEFAULT 'no-image.png',
+  `img` varchar(255) DEFAULT NULL,
   `is_offer` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -165,18 +170,20 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `category_id`, `title`, `content`, `price`, `old_price`, `description`, `keywords`, `img`, `is_offer`) VALUES
-(1, 1, 'knorr instant soup (100 gm)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '3.00', '0.00', 'knorr ', 'knorr ', '76.png', 0),
-(2, 1, 'chings noodles (75 gm)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '5.00', '8.00', NULL, NULL, '6.png', 1),
-(3, 1, 'lahsun sev (150 gm)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '3.00', '5.00', NULL, NULL, '7.png', 0),
-(4, 1, 'premium bake rusk (300 gm)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '5.00', '7.00', NULL, NULL, '8.png', 1),
-(5, 4, 'fresh spinach (palak)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '2.00', '3.00', NULL, NULL, '9.png', 0),
-(6, 5, 'fresh mango dasheri (1 kg)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '5.00', '8.00', NULL, NULL, '10.png', 1),
-(7, 5, 'fresh apple red (1 kg)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '6.00', '8.00', NULL, NULL, '11.png', 0),
-(8, 4, 'fresh broccoli (500 gm)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '4.00', '6.00', NULL, NULL, '12.png', 1),
-(9, 1, 'mixed fruit juice (1 ltr)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '3.00', '0.00', NULL, NULL, '13.png', 0),
-(10, 10, 'prune juice - sunsweet (1 ltr)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '4.00', '0.00', NULL, NULL, '14.png', 1),
-(11, 9, 'coco cola zero can (330 ml)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '3.00', '0.00', NULL, NULL, '15.png', 0),
-(12, 9, 'sprite bottle (2 ltr)', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '3.00', '0.00', NULL, NULL, '16.png', 1);
+(1, 1, 'knorr instant soup (100 gm)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n\r\n<p><strong>Excepteur </strong>sint occae cat <span style=\"color:#00FF00\">cupidatat </span>non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla <s>pariatur</s>.</p>\r\n', '3.00', '0.00', 'knorr ', 'knorr ', 'products/2021-04-03/6068736ab5342_76.png', 0),
+(2, 1, 'chings noodles (75 gm)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n', '5.00', '8.00', '', '', 'products/2021-04-03/6068737e79e8c_6.png', 1),
+(3, 1, 'lahsun sev (150 gm)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n', '3.00', '5.00', '', '', 'products/2021-04-03/606873986aa58_7.png', 1),
+(4, 1, 'premium bake rusk (300 gm)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n', '5.00', '7.00', '', '', 'products/2021-04-03/606873aadfd91_8.png', 1),
+(5, 4, 'fresh spinach (palak)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n', '2.00', '3.00', '', '', 'products/2021-04-03/606873bddafc7_9.png', 1),
+(6, 5, 'fresh mango dasheri (1 kg)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n', '5.00', '8.00', '', '', 'products/2021-04-03/606873d3340c2_10.png', 1),
+(7, 5, 'fresh apple red (1 kg)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n', '6.00', '8.00', '', '', 'products/2021-04-03/606873eebbee2_11.png', 1),
+(8, 4, 'fresh broccoli (500 gm)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n', '4.00', '6.00', '', '', 'products/2021-04-03/606874029a072_12.png', 1),
+(9, 1, 'mixed fruit juice (1 ltr)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n', '3.00', '0.00', '', '', 'products/2021-04-03/6068741337237_13.png', 0),
+(10, 10, 'prune juice - sunsweet (1 ltr)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n', '4.00', '0.00', '', '', 'products/2021-04-03/606874256394f_14.png', 0),
+(11, 9, 'coco cola zero can (330 ml)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n', '3.00', '0.00', '', '', 'products/2021-04-03/60687447e66f6_15.png', 0),
+(12, 9, 'sprite bottle (2 ltr)', '<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>\r\n', '3.00', '0.00', '', '', 'products/2021-04-03/60687460dd04a_16.png', 0),
+(20, 16, 'test product 3', '<p>test product 3</p>\r\n', '7.00', NULL, '', '', 'products/2021-04-03/60686ee53112b_4.jpg', 0),
+(21, 16, 'test product', '<p>111</p>\r\n', '0.00', NULL, '', '', 'products/2021-04-03/60687d1187df5_18.png', 0);
 
 -- --------------------------------------------------------
 
@@ -188,15 +195,17 @@ CREATE TABLE `user` (
   `id` int(10) UNSIGNED NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `auth_key` varchar(255) DEFAULT NULL
+  `auth_key` varchar(255) DEFAULT NULL,
+  `role` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `auth_key`) VALUES
-(1, 'admin@mail.ru', '$2y$13$LvCdAGrBrSgrniFllqBURePfk/BFsdxWBnu7ZcmtlqB4AOXXRwnce', 'L20EqIxIknDCd0NoavjAWFGGz7CKn_qi');
+INSERT INTO `user` (`id`, `username`, `password`, `auth_key`, `role`) VALUES
+(1, 'admin@mail.ru', '$2y$13$LvCdAGrBrSgrniFllqBURePfk/BFsdxWBnu7ZcmtlqB4AOXXRwnce', '6bRqHETETxbe7iIOcy1hn5nJU5LtiMRk', 10),
+(3, 'mail@mail.mail', '$2y$13$2TSGdheDIcV2n.E8EddaoeQQ2kCOlUjRYh/W6vsk37u2tLj6FwaY.', 'UG3aIJ00wCk7tTegkM9tf4sNPTf5UyKn', 1);
 
 --
 -- Индексы сохранённых таблиц
@@ -230,7 +239,8 @@ ALTER TABLE `product`
 -- Индексы таблицы `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -240,31 +250,31 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT для таблицы `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT для таблицы `order_product`
 --
 ALTER TABLE `order_product`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT для таблицы `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
